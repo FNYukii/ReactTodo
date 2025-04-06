d63c75cd5c3d9fa5430afe8714bceb3927ab1401
FNYukii <forestgreen14689@gmail.com>
2025-04-06 21:23:12 +0900

    feat: color設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ function App() {
   return (
     <div className="container mx-auto sm:px-0 px-4">
       <header className="py-4">
-        <span className="text-4xl">React Todo</span>
+        <span className="text-4xl font-light text-accent">React Todo</span>
       </header>
 
       <main>
         <h1 className="text-2xl font-bold">Todos</h1>
-        <p className="mt-2">Todoがありません</p>
+        <p className="mt-2 text-secondary">Todoがありません</p>
       </main>
     </div>
   )
